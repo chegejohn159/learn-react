26103a16bd25c657f5d0cef543e1e877c6c45389
chegejohn159 <chegejohn159@gmail.com>
2021-06-01 20:22:46 +0300

    bootstrap react repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<p>Hello Lauryn</p>
+
+<p>Kamau Said HI</p>
+    </div>
+  );
+}
+
+export default App;
